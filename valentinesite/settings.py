ce89da4cb1e84a90e65c6e78810564abfe484f02
Rohan Chaudhari <70095613+RohanChaudhari0301@users.noreply.github.com>
2026-02-12 21:46:15 +0530

    Add files via upload

--- valentinesite/settings.py
+++ valentinesite/settings.py
@@ -29,12 +29,14 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     "v-app-sobe.onrender.com",
+    "v-app-sobe.onrender.com/yes/",
     "localhost",
     "127.0.0.1"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://v-app-sobe.onrender.com"
+    "https://v-app-sobe.onrender.com",
+    "https://v-app-sobe.onrender.com/yes/"
 ]
 
 
